63c0b8134e302625f78ef8176cfb32712fef8876
Shahed Ahmed <shahed@shaheds-air.mynetworksettings.com>
2024-09-09 22:18:36 -0400

    Added All Features, Constants

--- Webpfizer/src/pages/patient.js
+++ Webpfizer/src/pages/patient.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Container, Grid, Paper } from '@mui/material';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  Grid,
+  Paper,
+} from "@mui/material";
+
+import { data } from "../constants/patient-data";
 
 const patient = () => {
   // Sample data
@@ -21,78 +31,162 @@ const patient = () => {
   const compareValues = (recorded, expected) => {
     return {
       fetalMovement: recorded.fetalMovement >= expected.fetalMovement,
-      uterineContraction: recorded.uterineContraction <= expected.uterineContraction,
-      baselineHeartRate: recorded.baselineHeartRate >= expected.baselineHeartRate,
-      abnormalityPercentage: recorded.abnormalityPercentage <= expected.abnormalityPercentage,
-      averageFetalValue: recorded.averageFetalValue >= expected.averageFetalValue,
+      uterineContraction:
+        recorded.uterineContraction <= expected.uterineContraction,
+      baselineHeartRate:
+        recorded.baselineHeartRate >= expected.baselineHeartRate,
+      abnormalityPercentage:
+        recorded.abnormalityPercentage <= expected.abnormalityPercentage,
+      averageFetalValue:
+        recorded.averageFetalValue >= expected.averageFetalValue,
     };
   };
 
-  const comparisonResults = compareValues(recordedData, recordedData.expectedData);
+  const comparisonResults = compareValues(
+    recordedData,
+    recordedData.expectedData
+  );
 
   return (
     <Container>
-      <AppBar >
+      <AppBar>
         <Toolbar>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             Patient Portal
           </Typography>
+          <Button color="inherit">Make Predictions</Button>
           <Button color="inherit">Schedule Appointment</Button>
           <Button color="inherit">Logout</Button>
         </Toolbar>
       </AppBar>
-      
-      <Typography variant="h4" sx={{ margin: '20px 0' }}>
+
+      <Typography variant="h4" sx={{ margin: "20px 0" }}>
         Recorded Data
       </Typography>
-      
+
       <Grid container spacing={3}>
+        {data.map(({ name, value, lowerBound, upperBound }) => (
+          <Grid item xs={12} sm={6} key={name}>
+            <Paper elevation={3} sx={{ padding: 2 }}>
+              <Typography variant="h5">
+                {name}: {value}
+              </Typography>
+              {!(lowerBound === 0 && upperBound === 0) && (
+                <>
+                  <Typography variant="body1">
+                    Expected: {lowerBound} - {upperBound}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color={
+                      value >= lowerBound && value <= upperBound
+                        ? "green"
+                        : "red"
+                    }
+                  >
+                    {value >= lowerBound && value <= upperBound
+                      ? "Within Expected Range"
+                      : "Below Expected Range"}
+                  </Typography>
+                </>
+              )}
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+
+      {/* <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
           <Paper elevation={3} sx={{ padding: 2 }}>
-            <Typography variant="h5">Fetal Movement: {recordedData.fetalMovement}</Typography>
-            <Typography variant="body1">Expected: {recordedData.expectedData.fetalMovement}</Typography>
-            <Typography variant="body2" color={comparisonResults.fetalMovement ? "green" : "red"}>
-              {comparisonResults.fetalMovement ? "Within Expected Range" : "Below Expected Range"}
+            <Typography variant="h5">
+              Fetal Movement: {recordedData.fetalMovement}
+            </Typography>
+            <Typography variant="body1">
+              Expected: {recordedData.expectedData.fetalMovement}
+            </Typography>
+            <Typography
+              variant="body2"
+              color={comparisonResults.fetalMovement ? "green" : "red"}
+            >
+              {comparisonResults.fetalMovement
+                ? "Within Expected Range"
+                : "Below Expected Range"}
             </Typography>
           </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper elevation={3} sx={{ padding: 2 }}>
-            <Typography variant="h5">Uterine Contraction: {recordedData.uterineContraction}</Typography>
-            <Typography variant="body1">Expected: {recordedData.expectedData.uterineContraction}</Typography>
-            <Typography variant="body2" color={comparisonResults.uterineContraction ? "green" : "red"}>
-              {comparisonResults.uterineContraction ? "Within Expected Range" : "Above Expected Range"}
+            <Typography variant="h5">
+              Uterine Contraction: {recordedData.uterineContraction}
+            </Typography>
+            <Typography variant="body1">
+              Expected: {recordedData.expectedData.uterineContraction}
+            </Typography>
+            <Typography
+              variant="body2"
+              color={comparisonResults.uterineContraction ? "green" : "red"}
+            >
+              {comparisonResults.uterineContraction
+                ? "Within Expected Range"
+                : "Above Expected Range"}
             </Typography>
           </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper elevation={3} sx={{ padding: 2 }}>
-            <Typography variant="h5">Baseline Heart Rate: {recordedData.baselineHeartRate}</Typography>
-            <Typography variant="body1">Expected: {recordedData.expectedData.baselineHeartRate}</Typography>
-            <Typography variant="body2" color={comparisonResults.baselineHeartRate ? "green" : "red"}>
-              {comparisonResults.baselineHeartRate ? "Within Expected Range" : "Below Expected Range"}
+            <Typography variant="h5">
+              Baseline Heart Rate: {recordedData.baselineHeartRate}
+            </Typography>
+            <Typography variant="body1">
+              Expected: {recordedData.expectedData.baselineHeartRate}
+            </Typography>
+            <Typography
+              variant="body2"
+              color={comparisonResults.baselineHeartRate ? "green" : "red"}
+            >
+              {comparisonResults.baselineHeartRate
+                ? "Within Expected Range"
+                : "Below Expected Range"}
             </Typography>
           </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper elevation={3} sx={{ padding: 2 }}>
-            <Typography variant="h5">Abnormality Percentage: {recordedData.abnormalityPercentage}%</Typography>
-            <Typography variant="body1">Expected: {recordedData.expectedData.abnormalityPercentage}%</Typography>
-            <Typography variant="body2" color={comparisonResults.abnormalityPercentage ? "green" : "red"}>
-              {comparisonResults.abnormalityPercentage ? "Within Expected Range" : "Above Expected Range"}
+            <Typography variant="h5">
+              Abnormality Percentage: {recordedData.abnormalityPercentage}%
+            </Typography>
+            <Typography variant="body1">
+              Expected: {recordedData.expectedData.abnormalityPercentage}%
+            </Typography>
+            <Typography
+              variant="body2"
+              color={comparisonResults.abnormalityPercentage ? "green" : "red"}
+            >
+              {comparisonResults.abnormalityPercentage
+                ? "Within Expected Range"
+                : "Above Expected Range"}
             </Typography>
           </Paper>
         </Grid>
         <Grid item xs={12}>
           <Paper elevation={3} sx={{ padding: 2 }}>
-            <Typography variant="h5">Average Fetal Value: {recordedData.averageFetalValue}</Typography>
-            <Typography variant="body1">Expected: {recordedData.expectedData.averageFetalValue}</Typography>
-            <Typography variant="body2" color={comparisonResults.averageFetalValue ? "green" : "red"}>
-              {comparisonResults.averageFetalValue ? "Within Expected Range" : "Below Expected Range"}
+            <Typography variant="h5">
+              Average Fetal Value: {recordedData.averageFetalValue}
+            </Typography>
+            <Typography variant="body1">
+              Expected: {recordedData.expectedData.averageFetalValue}
+            </Typography>
+            <Typography
+              variant="body2"
+              color={comparisonResults.averageFetalValue ? "green" : "red"}
+            >
+              {comparisonResults.averageFetalValue
+                ? "Within Expected Range"
+                : "Below Expected Range"}
             </Typography>
           </Paper>
         </Grid>
-      </Grid>
+      </Grid> */}
     </Container>
   );
 };
